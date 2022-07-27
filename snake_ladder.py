a16606743856fa6591806d6681c173df280d6516
Elavarasu1107 <sellamuthuappusamy@gmail.com>
2022-07-27 13:29:22 +0530

    [Elavarasu] UC4-Repeat till player reach winning position

--- snake_ladder.py
+++ snake_ladder.py
@@ -11,17 +11,19 @@ def snake_ladder():
     is_no_play = 0
     is_snake = 1
     is_ladder = 2
-    player_option = random.randint(0, 2)
-    dice_value = random.randint(1, 6)
+    winning_position = 100
 
-    if player_option == is_ladder:
-        player_position += dice_value
-    elif player_option == is_snake:
-        player_position -= dice_value
-        if player_position < 0:
-            player_position = 0
-    else:
-        player_position += is_no_play
+    while player_position < winning_position:
+        player_option = random.randint(0, 2)
+        dice_value = random.randint(1, 6)
+        if player_option == is_ladder:
+            player_position += dice_value
+        elif player_option == is_snake:
+            player_position -= dice_value
+            if player_position < 0:
+                player_position = 0
+        else:
+            player_position += is_no_play
 
     print("Player Position: ", player_position)
 
