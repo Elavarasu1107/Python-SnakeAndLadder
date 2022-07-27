93af2dbe303e75213c2d8ba33d4d667a4304ecfb
Elavarasu1107 <sellamuthuappusamy@gmail.com>
2022-07-27 13:24:59 +0530

    [Elavarasu] UC3-Check player option

--- snake_ladder.py
+++ snake_ladder.py
@@ -8,9 +8,22 @@ def snake_ladder():
     :return: None
     """
     player_position = 0
+    is_no_play = 0
+    is_snake = 1
+    is_ladder = 2
+    player_option = random.randint(0, 2)
     dice_value = random.randint(1, 6)
+
+    if player_option == is_ladder:
+        player_position += dice_value
+    elif player_option == is_snake:
+        player_position -= dice_value
+        if player_position < 0:
+            player_position = 0
+    else:
+        player_position += is_no_play
+
     print("Player Position: ", player_position)
-    print("Dice Value: ", dice_value)
 
 
 if __name__ == '__main__':
