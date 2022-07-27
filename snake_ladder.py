dd93a2f2be981173a32b180b91e464c781f8b19b
Elavarasu1107 <sellamuthuappusamy@gmail.com>
2022-07-27 13:16:27 +0530

    [Elavarasu] UC1-Player Position

---
+++ snake_ladder.py
@@ -0,0 +1,15 @@
+import random
+
+
+def snake_ladder():
+    """
+    This function is a simple snake and ladder game.
+    If a player reaches the exact position of 100, printed as player won the game
+    :return: None
+    """
+    player_position = 0
+    print("Player Position: ", player_position)
+
+
+if __name__ == '__main__':
+    snake_ladder()
